6e1290bd5fc203f3ea3a66b47c5fe94c9b82cbfe
Иван Жуков <ivanzukov@MacBook-Air-Ivan-4.local>
2023-11-20 23:48:57 +0300

    change styles

--- src/components/ProfileBlock/ProfileBlock.jsx
+++ src/components/ProfileBlock/ProfileBlock.jsx
@@ -2,7 +2,7 @@ import styles from './ProfileBlock.module.scss'
 const ProfileBlock = ({data}) => {
  return <section className={styles.wrapper}>
 	<div className={styles.img}>
-		<img src="/public/2023-10-11 00.19.56.jpg" alt="profile" />
+		<img src="/public/avatar.png" alt="profile" />
 	</div>
 	<div className={styles.info_block}>
 		<div className={styles.name}>{data.data?.name}</div>
